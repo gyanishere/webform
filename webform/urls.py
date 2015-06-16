3b7eff04e32e1dac79ab17551d195fdf3a349a32
Gyan Prayaga <reachgyan@gmail.com>
2015-06-16 13:49:55 -0700

    Added urls.py for member_form and added index view

--- webform/urls.py
+++ webform/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'webform.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^member_form/', include('member_form.urls', namespace='member_form')),
     url(r'^admin/', include(admin.site.urls)),
 )
