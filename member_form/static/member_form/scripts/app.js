c51b86bb56ceac4e18b33c5d45c15e6b1ff4a334
Antonio Dangond <antoniodangond@gmail.com>
2015-06-24 15:37:53 -0700

    Changes variable names in accordance with JS naming convention.

--- member_form/static/member_form/scripts/app.js
+++ member_form/static/member_form/scripts/app.js
@@ -4,41 +4,42 @@
 
 $('.container__form').on('submit',function() {
 	// grab data from all inputs
-	var first_name = $('#first_name').val();
-	var last_name = $('#last_name').val();
+	var firstName = $('#first_name').val();
+	var lastName = $('#last_name').val();
 	var city = $('#city').val();
 	var state = $('#state').val();
+	// TODO: remove line below if not necessary
 	if (!state) {state = false;} // otherwise would return Null, this prevents inconsistencies in data output (since all the other inputs return False)
 	var zip = $('#zip').val();
 	var timezone = $('#timezone').val();
-	var mobile_number = $('#mobile_number').val();
+	var mobileNumber = $('#mobile_number').val();
 	var birthdate = $('#birthdate').val();
 
 	// individual checkboxes
-	var infant_toddler = $('label[for="checkboxG4"]').hasClass('chk')
+	var infantToddler = $('label[for="checkboxG4"]').hasClass('chk')
 	var child = $('label[for="checkboxG5"]').hasClass('chk')
 	var teen = $('label[for="checkboxG6"]').hasClass('chk')
-	var young_adult = $('label[for="checkboxG1"]').hasClass('chk')
+	var youngAdult = $('label[for="checkboxG1"]').hasClass('chk')
 	var adult = $('label[for="checkboxG2"]').hasClass('chk')
-	var older_adult = $('label[for="checkboxG3"]').hasClass('chk')
+	var olderAdult = $('label[for="checkboxG3"]').hasClass('chk')
 	// (?) seems repetitive, but I couldn't think of a more scalable solution (maybe a For loop)
 
 	// create dictionary with data
 	formObject = {
-		firstName: first_name,
-		lastName: last_name,
+		firstName: firstName,
+		lastName: lastName,
 		city: city,
 		state: state,
 		zip: zip,
 		timeZone: timezone,
-		mobileNumber: mobile_number,
+		mobileNumber: mobileNumber,
 		birthDate: birthdate,
-		infantToddler: infant_toddler,
+		infantToddler: infantToddler,
 		child: child,
 		teen: teen,
-		youngAdult: young_adult,
+		youngAdult: youngAdult,
 		adult: adult,
-		olderAdult: older_adult,
+		olderAdult: olderAdult,
 	}
 
 	// call ajax function
@@ -47,15 +48,13 @@ $('.container__form').on('submit',function() {
 	return false;
 	// (?) return false works as event.stopPropagation() and event.preventDefault() within jQuery event handler
 
-  });
+});
 
-  function post() {
-	/*  $.post(formObject, function(data) {
-			console.log(data);
-		});
-	*/
-	console.log(formObject) 
-	// debug
-  }
-
-  
+function post() {
+/*  $.post(formObject, function(data) {
+		console.log(data);
+	});
+*/
+console.log(formObject)
+// debug
+}
